b01f3a369448ffa855489722d74bc7a3cb0d13c8
Robert Girardin <robertlgirardin@gmail.com>
2020-06-15 11:30:29 -0800

    Updates

---
+++ precache-manifest.e2d61d301816d03bb9129672467872fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cbc2d27ba3a399db4b0eece3011328",
+    "url": "/MMXX/index.html"
+  },
+  {
+    "revision": "8477335f3fdc8f79e281",
+    "url": "/MMXX/static/css/2.5c1d7e3b.chunk.css"
+  },
+  {
+    "revision": "435e55a80d158623b5fc",
+    "url": "/MMXX/static/css/main.b861d80a.chunk.css"
+  },
+  {
+    "revision": "8477335f3fdc8f79e281",
+    "url": "/MMXX/static/js/2.a24dd173.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/MMXX/static/js/2.a24dd173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "435e55a80d158623b5fc",
+    "url": "/MMXX/static/js/main.47c604fd.chunk.js"
+  },
+  {
+    "revision": "af6795748fed94108403",
+    "url": "/MMXX/static/js/runtime-main.a912ab4c.js"
+  },
+  {
+    "revision": "4f04d7fbf30c8b366cdc499e010d3f95",
+    "url": "/MMXX/static/media/mmxx_logo.4f04d7fb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MMXX/precache-manifest.4363bd30f873071549ac2f23e77e7cc3.js"
+  "/MMXX/precache-manifest.e2d61d301816d03bb9129672467872fc.js"
 );
 
 self.addEventListener('message', (event) => {
